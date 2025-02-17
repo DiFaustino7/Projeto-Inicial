29c9f0bc19d09fc3727003b69e79ece03bf57f90
diego faustino <diegofaustino576@gmail.com>
2025-02-17 20:24:21 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,24 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle("light")
+    //tambem poderia ser assim:
+
+    //if(html.classList.contains(`light`)){
+    //    html.classList.remove(`light`)
+    //} else{
+    //    html.classList.add(`light`)
+    //}
+
+
+    //para trocar imagem quando estiver no light.
+    //pegar a tag img
+    //substituir a imagem
+    //se tiver light mode, adicionar imagem light
+    //se tiver sem light mode, manter a imagem normal
+    const img=document.querySelector(`#profile img`)
+    if (html.classList.contains(`light`)){
+        img.setAttribute(`src`,`./assets/assets/avatar-light.png`)
+    } else{
+        img.setAttribute(`src`,`./assets/assets/avatar.png`)
+    }
+}
